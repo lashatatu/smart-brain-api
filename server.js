865befc43b62dbcd20e5be0d0c408f6a94fb13b1
lasha tatulashvili <lashatatu@gmail.com>
2022-01-18 01:18:11 +0400

    bcrypt-nodejs

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
-
-app.use(express.json());
 const database = {
   users: [
     {
       id: "123",
       name: "John",
       email: "john@gmail.com",
+      password: "cookies",
       entries: 0,
       joined: new Date(),
     },
@@ -17,11 +17,16 @@ const database = {
       id: "124",
       name: "sally",
       email: "sally@gmail.com",
+      password: "bananas",
       entries: 0,
       joined: new Date(),
     },
   ],
 };
+
+app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(database.users);
@@ -41,14 +46,10 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null,null, (err, hash) => {
-    console.log(hash);
-  });
   database.users.push({
     id: "125",
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date(),
   });
@@ -69,13 +70,13 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
-app.post('/image',(req,res)=>{
+app.post("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach((user) => {
     if (user.id === id) {
       found = true;
-      user.entries++
+      user.entries++;
       return res.json(user.entries);
     }
   });
@@ -83,20 +84,8 @@ app.post('/image',(req,res)=>{
   if (!found) {
     res.status(404).json("no user found");
   }
-})
-
-bcrypt.hash("bacon", null,null, (err, hash) => {
-  console.log(hash);
 });
 
-bcrypt.compare("bacon", hash, (err, res) => {
-  console.log(res);
-});
-
-bcrypt.compare("veggies", hash, (err, res) => {
-  console.log(res);
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
