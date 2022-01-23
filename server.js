2548ec5156b88033393a05d93707f61714750e3f
lasha tatulashvili <lashatatu@gmail.com>
2022-01-23 16:08:04 +0400

    bcrypt-nodejs

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex=require("knex")
+
+const db=knex({
+  client:"pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "heydrich",
+    database: "smart-brain"
+  }
+});
+
+db.select("*").from("users").then(data=>{
+  console.log(data);
+});
 
 const database = {
   users: [
@@ -46,14 +61,17 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
+  db('users')
+  .returning('*')
+  .insert({
     email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+    name: name,
+    joined: new Date()
+  })
+    .then(user=>{
+      res.json(user[0])
+  })
+    .catch(err=>res.status(400).json('unable to register'))
 });
 
 app.get("/profile/:id", (req, res) => {
