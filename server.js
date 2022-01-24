9efd150f32fd7fa713027e57c575b1405ec9dd8a
lasha tatulashvili <lashatatu@gmail.com>
2022-01-24 08:21:33 +0400

    cleanup

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
-const register = require("./controllers/register");
+
 const signin = require("./controlers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
+const register = require("./controlers/register");
+const profile = require("./controlers/profile");
+const image = require("./controlers/image");
 
 const db = knex({
   client: "pg",
@@ -44,6 +45,9 @@ app.get("/profile/:id", (req, res) => {
 // image
 app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
 });
 
 app.listen(3001, () => {
